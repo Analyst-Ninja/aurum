84239b8548aeefe73364f41327695de4357c1750
Rohit Kumar <r.kumar01@hotmail.com>
2026-08-31 17:33:47 +0530

    RENAME: the income_stmts feed name for filw

--- src/ingestion/runner.py
+++ src/ingestion/runner.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from src.ingestion.factory import factory
 from src.ingestion.feed.base_feed import BaseFeed
 from src.utils.config_reader import read_config
-from src.ingestion.feed import stock_market, income_stmts
+from src.ingestion.feed import stock_market, income_stmts_feed
 from src.ingestion.datasources.api.yahoo import ohlcv
 from src.ingestion.datasources.api.edgar import income_stmts
 from src.ingestion.datasources.storage import db
